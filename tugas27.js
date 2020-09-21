1ec944f05861e76bcd6cd2fe0f41bc74f5573e79
tasyaSukma <maftuhah.tasya@gmail.com>
2020-09-21 13:07:41 +0700

    Update tugas27.js

--- tugas27.js
+++ tugas27.js
@@ -1,13 +1,13 @@
-var alamat1 = ["No","rumah saya", 30, "A"]
+var alamat1 = ["No","rumah saya", 30,"A"]
 var alamat2 = ["Jalan pangeran Tirtayasa No", 70]
 var strAlamat1 = "", strAlamat2 = ""
 
 for(var i = 0; i<alamat1.length; i++){
-	strAlamat1 = strAlamat1 + alamat1[i] + " ";
+	strAlamat1 = strAlamat1.concat(alamat1[i]," ");
 }
 
 for(var i = 0; i<alamat2.length; i++){
-	strAlamat2 = strAlamat2 + alamat2[i] + " ";
+	strAlamat2 = strAlamat2.concat( alamat2[i]," ");
 }
 
 console.log(strAlamat1)
